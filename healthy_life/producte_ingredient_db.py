5e3749e689b3d9090b77ded96301f05798d5a6b6
GerardGallardo <81705580+GerardGallardo@users.noreply.github.com>
2021-12-07 15:51:29 +0100

    Update producte_ingredient_db.py

--- healthy_life/producte_ingredient_db.py
+++ healthy_life/producte_ingredient_db.py
@@ -1,1 +1,25 @@
+class ProducteIngredientDB:
 
+  """ Init """
+  def __init__(self, path):
+    self._db = pd.read_csv(path, index_col=0)
+    self._db_backup = self._db.copy()
+    self._path = path
+
+  """ Method """
+  def add_producte_ingredient(self, id_producte, id_ingredient):
+    id_series = pd.Series([id_producte, id_ingredient], index = self._db.columns)
+    self._db = self._db.append(id_series, ignore_index=True)
+    self.save()
+
+  """ Getters """
+  def get_ingredients(self, id_producte):
+    return self._db[self._db.id_producte == id_producte]["id_ingredient"]
+
+  """ Show database """
+  def show(self):
+    display(self._db)
+
+  """ Save database """
+  def save(self):
+    self._db.to_csv(self._path)
