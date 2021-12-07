c7af75a30034a5f9550d0b4e54d201fef08de61a
Gerard Jover Pujol <64100950+IararIV@users.noreply.github.com>
2021-12-07 15:58:10 +0100

    Add files via upload
    Fitxer main que treballa amb els mòduls implementats

---
+++ healthy_life/main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+from productes_db import ProductesDB
+from ingredients_db import IngredientsDB
+from producte_ingredient_db import ProducteIngredientDB
+from receptes_db import ReceptesDB
+#from usuaris_db import UsuarisDB
+#from ingredients_recepta_db import IngredientsReceptaDB
+#from valoracions_db import ValoracionsDB
+#from rebost_db import RebostDB
+
+
+if __name__ == '__main__':
+    print("Projecte Healthy_Life!")
+
+    print("Test productes_db...")
+    
